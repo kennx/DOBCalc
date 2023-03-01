5c49718d2326029040215c0b2234203a3f2967b5
Kenn Zh <kenn@Kenns-Mac-mini.local>
2023-03-01 19:57:29 +0800

    first commit

---
+++ app/src/main/java/cc/niaoer/dobcalc/MainActivity.kt
@@ -0,0 +1,14 @@
+package cc.niaoer.dobcalc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cc.niaoer.dobcalc.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
